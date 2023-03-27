71f03aec257cc7ed1b9644b204fe829239c5e4c1
Cleante Aubert <99390720+Cleante-Aubert@users.noreply.github.com>
2023-03-27 13:58:37 +0200

    Classe voiture fini

--- qualiteDeDeveloppement-main/src/main/java/com/example/demo/data/Voiture.java
+++ qualiteDeDeveloppement-main/src/main/java/com/example/demo/data/Voiture.java
@@ -8,7 +8,31 @@ import javax.persistence.Id;
 @Entity
 public class Voiture {
     int id;
+    private String marque;
+    private int prix;
 
+
+    // Constructeurs
+    public Voiture(){}
+
+    public Voiture(String marque, int prix){ this.marque=marque;  this.prix=prix; }
+
+    // Extention conseillée pour éviter de faire tt les getter, setter et constructeurs !!
+    lombock
+
+    // Set et get marque
+    private String getMarque(){ return marque; }
+
+    private void setMarque(String marque){ this.marque = marque; }
+
+    // Set et get prix
+    private int getPrix(){ return prix; }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+    // Set et get id
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     public int getId() {
